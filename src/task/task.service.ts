945007f2eedb6d7eed432f6fc238157dda0af589
Jhonatan Azevedo <dev.azevedo@outlook.com>
2025-02-07 11:40:53 -0300

    ADD: Project

---
+++ src/task/task.service.ts
@@ -0,0 +1,54 @@
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { FindAllParameters, TaskDto } from './task.dto';
+
+@Injectable()
+export class TaskService {
+    private tasks: TaskDto[] = [];
+
+    create(task: TaskDto) {
+        this.tasks.push(task);
+        console.log(this.tasks);
+    }
+
+    findById(id: string): TaskDto {
+        const task = this.tasks.find(task => task.id === id);
+
+        if (!task)
+            throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+
+        return task;
+    }
+
+    findAll(params: FindAllParameters): TaskDto[] {
+        return this.tasks.filter(task => {
+            let match = true;
+
+            if (params.title !== undefined && !task.title.includes(params.title))
+                match = false;
+
+            if (params.status !== undefined && !task.status.includes(params.status))
+                match = false;
+
+            return match;
+        });
+    }
+
+    update(task: TaskDto) {
+        const index = this.tasks.findIndex(task => task.id === task.id);
+
+        if (index == -1)
+            throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+
+        this.tasks[index] = task;
+    }
+
+    remove(id: string) {
+        const index = this.tasks.findIndex(task => task.id === id);
+
+        if (index == -1)
+            throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+
+        this.tasks.splice(index, 1);
+    }
+
+}
